499285933e366128e697d80d194d4c993ad9974c
NassarX <mahmoud@nassarx.com>
2022-12-10 03:15:20 +0000

    Fix issues and enhance schema structure

--- retail-chains.sql
+++ retail-chains.sql
@@ -3,46 +3,39 @@ CREATE DATABASE IF NOT EXISTS `retail_chains`;
 
 USE `retail_chains`;
 
-DROP TABLE IF EXISTS `region`;
-CREATE TABLE IF NOT EXISTS `region` (
-  id TINYINT AUTO_INCREMENT NOT NULL,
-  region_name VARCHAR(25) NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 DROP TABLE IF EXISTS `country`;
 CREATE TABLE IF NOT EXISTS `country` (
-  id VARCHAR(2) NOT NULL,
-  country_name VARCHAR(40) NULL,
-  region_id TINYINT NULL,
-  PRIMARY KEY (id),
-  CONSTRAINT fk_country_region_id FOREIGN KEY (region_id) references region (id)
+  id int NOT NULL,
+  name VARCHAR(40) NULL,
+  code CHAR(4) NULL ,
+  language VARCHAR(10) NULL ,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `location`;
 CREATE TABLE IF NOT EXISTS `location` (
   id INT AUTO_INCREMENT NOT NULL ,
-  street_address VARCHAR(40) NULL,
+  street_address VARCHAR(255) NULL,
   postal_code VARCHAR(12) NULL,
   city VARCHAR(30) NOT NULL,
-  state_province VARCHAR(25) NULL,
-  country_id VARCHAR(2) NULL,
+  region VARCHAR(25) NULL,
+  country_id INT NOT NULL,
   PRIMARY KEY (id),
-  CONSTRAINT location_country_id FOREIGN KEY (country_id) references country (id)
+  CONSTRAINT location_country_id FOREIGN KEY (country_id) references `country` (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `retail_chain`;
 CREATE TABLE IF NOT EXISTS `retail_chain` (
     id INT AUTO_INCREMENT NOT NULL ,
-    retail_chain_name VARCHAR(30) NOT NULL,
+    name VARCHAR(30) NOT NULL,
     industry VARCHAR(30) NOT NULL ,
     founded_year VARCHAR(10),
     headquarter VARCHAR(10),
-    revenue decimal(6,0),
+    revenue VARCHAR(100),
     url TEXT NULL,
-    country_id VARCHAR(2) NULL,
+    country_id INT NOT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT retail_chain_country_id FOREIGN KEY (country_id) references country (id)
+    CONSTRAINT retail_chain_country_id FOREIGN KEY (country_id) references `country` (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `store`;
@@ -52,8 +45,8 @@ CREATE TABLE IF NOT EXISTS `store` (
   retail_chain_id INT NULL,
   location_id INT NULL,
   PRIMARY KEY (id),
-  CONSTRAINT store_retail_chain_id FOREIGN KEY (retail_chain_id) references retail_chain (id),
-  CONSTRAINT store_location_id FOREIGN KEY (location_id) references location (id)
+  CONSTRAINT store_retail_chain_id FOREIGN KEY (retail_chain_id) references `retail_chain` (id),
+  CONSTRAINT store_location_id FOREIGN KEY (location_id) references `location` (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `job`;
@@ -73,20 +66,23 @@ CREATE TABLE IF NOT EXISTS `employee` (
   email VARCHAR(25) NOT NULL,
   phone_number VARCHAR(20) NULL,
   hire_date date NOT NULL,
-  salary decimal(8,2) NULL,
-  commission_pct decimal(2,2) NULL,
   store_id INT NOT NULL,
   job_id INT NOT NULL,
   PRIMARY KEY (id),
-  CONSTRAINT fk_employee_store_id FOREIGN KEY (store_id) references store (id),
-  CONSTRAINT fk_employee_job_id FOREIGN KEY (job_id) references job (id)
+  CONSTRAINT fk_employee_store_id FOREIGN KEY (store_id) references `store` (id),
+  CONSTRAINT fk_employee_job_id FOREIGN KEY (job_id) references `job` (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `supplier`;
 CREATE TABLE IF NOT EXISTS `supplier`(
 	id INT AUTO_INCREMENT NOT NULL,
 	name VARCHAR(100),
-    PRIMARY KEY (id)
+	contact_name VARCHAR(100),
+	contact_title VARCHAR(100),
+	phone VARCHAR(25),
+	location_id INT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT supplier_location_id FOREIGN KEY (location_id) references `location` (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `category`;
@@ -107,13 +103,14 @@ CREATE TABLE IF NOT EXISTS `product`(
     category_id INT NOT NULL,
     price DECIMAL(10,2) NOT NULL,
     supplier_id INT NOT NULL,
+    rate DECIMAL(2, 1) DEFAULT 0,
     PRIMARY KEY (id),
-    CONSTRAINT fk_product_category_od FOREIGN KEY (category_id) REFERENCES category(id),
-    CONSTRAINT fk_product_supplier_id FOREIGN KEY (supplier_id) REFERENCES supplier(id)
+    CONSTRAINT fk_product_category_od FOREIGN KEY (category_id) REFERENCES `category`(id),
+    CONSTRAINT fk_product_supplier_id FOREIGN KEY (supplier_id) REFERENCES `supplier`(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `supply_history`;
-CREATE TABLE IF NOT EXISTS `supply_history`(
+DROP TABLE IF EXISTS `supply_process`;
+CREATE TABLE IF NOT EXISTS `supply_process`(
     supplier_id INT NOT NULL,
 	product_id INT NOT NULL,
 	store_id INT NOT NULL,
@@ -122,24 +119,23 @@ CREATE TABLE IF NOT EXISTS `supply_history`(
 	created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME,
 	PRIMARY KEY (supplier_id, product_id, store_id, date),
-	CONSTRAINT fk_supply_history_supplier_id FOREIGN KEY (supplier_id) REFERENCES supplier(id),
-	CONSTRAINT fk_supply_history_product_id FOREIGN KEY (product_id) REFERENCES product(id),
-	CONSTRAINT fk_supply_history_store_id FOREIGN KEY (store_id) REFERENCES store(id)
+	CONSTRAINT fk_supply_process_supplier_id FOREIGN KEY (supplier_id) REFERENCES `supplier`(id),
+	CONSTRAINT fk_supply_process_product_id FOREIGN KEY (product_id) REFERENCES `product`(id),
+	CONSTRAINT fk_supply_process_store_id FOREIGN KEY (store_id) REFERENCES `store`(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `store_product`;
-CREATE TABLE IF NOT EXISTS `store_product`(
+DROP TABLE IF EXISTS `store_products`;
+CREATE TABLE IF NOT EXISTS `store_products`(
     id INT AUTO_INCREMENT,
 	store_id INT NOT NULL ,
     product_id INT NOT NULL ,
-    date_of_expire DATE,
-    is_expired BOOLEAN NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
     quantity INT DEFAULT 0 NOT NULL,
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME,
     PRIMARY KEY (id),
-    CONSTRAINT fk_store_product_store_id FOREIGN KEY (store_id) REFERENCES store(id),
-    CONSTRAINT fk_store_product_product_id FOREIGN KEY (product_id) REFERENCES product(id)
+    CONSTRAINT fk_store_products_store_id FOREIGN KEY (store_id) REFERENCES `store`(id),
+    CONSTRAINT fk_store_products_product_id FOREIGN KEY (product_id) REFERENCES `product`(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `customer`;
@@ -155,8 +151,8 @@ CREATE TABLE IF NOT EXISTS `customer`(
 	PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `store_order`;
-CREATE TABLE IF NOT EXISTS `store_order` (
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE IF NOT EXISTS `orders` (
   id INT AUTO_INCREMENT NOT NULL,
   customer_id INT NOT NULL,
   store_id INT NOT NULL,
@@ -166,26 +162,28 @@ CREATE TABLE IF NOT EXISTS `store_order` (
   payment_method ENUM('Cash','Net Banking','Credit Card','Debit Card') DEFAULT 'Cash' NOT NULL,
   type ENUM('Online', 'Store'),
   amount DECIMAL(10,2) NOT NULL DEFAULT 0,
-  status VARCHAR(25) NOT NULL DEFAULT 'Yet to be delivered',
+  status ENUM('Pending', 'InProgress', 'Done') DEFAULT 'Pending',
+  employee_id INT NOT NULL,
   created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME,
   PRIMARY KEY (id),
-  CONSTRAINT fk_order_customer_id FOREIGN KEY (customer_id) REFERENCES customer(id),
-  CONSTRAINT fk_order_store_id FOREIGN KEY (store_id) REFERENCES store(id)
+  CONSTRAINT fk_order_customer_id FOREIGN KEY (customer_id) REFERENCES `customer`(id),
+  CONSTRAINT fk_order_store_id FOREIGN KEY (store_id) REFERENCES `store`(id),
+  CONSTRAINT FOREIGN KEY fk_order_employee_id(employee_id) REFERENCES `employee`(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `order_product`;
-CREATE TABLE IF NOT EXISTS `order_product` (
+DROP TABLE IF EXISTS `order_products`;
+CREATE TABLE IF NOT EXISTS `order_products` (
   order_id INT NOT NULL,
-  product_id INT NOT NULL,
+  store_product_id INT NOT NULL,
   quantity INT NOT NULL,
-  unit_price DECIMAL(10,2) NOT NULL,
-  amount DECIMAL(10,2) NOT NULL,
+  unit_price DECIMAL(10,2) NOT NULL DEFAULT 0,
+  amount DECIMAL(10,2) NOT NULL DEFAULT 0,
   created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME,
-  PRIMARY KEY (order_id, product_id),
-  CONSTRAINT fk_order_product_store_order_id FOREIGN KEY (order_id) references store_order (id),
-  CONSTRAINT fk_order_product_store_product_id FOREIGN KEY (product_id) references store_product (id)
+  PRIMARY KEY (order_id, store_product_id),
+  CONSTRAINT fk_order_products_order_id FOREIGN KEY (order_id) references `orders`(id),
+  CONSTRAINT fk_order_products_store_products_id FOREIGN KEY (store_product_id) references `store_products`(product_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `transaction`;
@@ -195,15 +193,15 @@ CREATE TABLE IF NOT EXISTS `transaction` (
 	store_id INT NOT NULL,
 	order_id INT NOT NULL,
 	amount DECIMAL(10,2),
-	date_of_purchase date not null,
-	payment_method varchar(40),
-	person_id int ,
-	employee_person_id int ,
+	date_of_purchase DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	payment_method VARCHAR(40),
+	employee_id INT NOT NULL,
 	PRIMARY KEY(id),
 	UNIQUE (customer_id, store_id, order_id),
-	CONSTRAINT FOREIGN KEY fk_transaction_customer_id(customer_id) REFERENCES customer(id),
-	CONSTRAINT FOREIGN KEY fk_transaction_store_id(store_id) REFERENCES store(id),
-	CONSTRAINT FOREIGN KEY fk_transaction_store_order_id(order_id) REFERENCES store_order(id)
+	CONSTRAINT FOREIGN KEY fk_transaction_customer_id(customer_id) REFERENCES `customer`(id),
+	CONSTRAINT FOREIGN KEY fk_transaction_store_id(store_id) REFERENCES `store`(id),
+	CONSTRAINT FOREIGN KEY fk_transaction_order_id(order_id) REFERENCES `orders`(id),
+	CONSTRAINT FOREIGN KEY fk_transaction_employee_id(employee_id) REFERENCES `employee`(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `product_rate`;
@@ -211,11 +209,23 @@ CREATE TABLE IF NOT EXISTS `product_rate`(
     id INT AUTO_INCREMENT,
     product_id INT NOT NULL ,
     customer_id INT NOT NULL ,
-    rate DOUBLE,
-    is_expired BOOLEAN NOT NULL,
+    rate DECIMAL(2, 1),
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME,
     PRIMARY KEY (id),
-    CONSTRAINT fk_product_rate_product_id FOREIGN KEY (product_id) REFERENCES product(id),
-    CONSTRAINT fk_product_rate_customer_id FOREIGN KEY (customer_id) REFERENCES customer(id)
+    UNIQUE (customer_id, product_id),
+    CONSTRAINT fk_product_rate_product_id FOREIGN KEY (product_id) REFERENCES `product`(id),
+    CONSTRAINT fk_product_rate_customer_id FOREIGN KEY (customer_id) REFERENCES `customer`(id),
+    CONSTRAINT chk_Ratings CHECK (rate >= 0 AND rate <= 5)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `logs`;
+CREATE TABLE IF NOT EXISTS `logs`(
+    id BIGINT unsigned AUTO_INCREMENT,
+    l_table VARCHAR(250) ,
+    l_action VARCHAR(250) ,
+    row_identifier VARCHAR(250) ,
+    new_value TEXT,
+    logged_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
